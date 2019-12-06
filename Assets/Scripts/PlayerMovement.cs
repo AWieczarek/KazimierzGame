1f0f1d0fb077a8496043675eb687ed7b9f5edad6
AWieczarek <adamwie12@gmail.com>
2019-12-06 17:50:54 +0100

    Add simple movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(0, 0, 1);
+        Vector3 velocity = direction * speed;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
