8a9d96d112ff80817ddfa3182b968d0994bee511
AWieczarek <adamwie12@gmail.com>
2020-04-23 21:41:33 +0200

    Test

--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -7,7 +7,7 @@ public class OptionsMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Kupa");
     }
 
     // Update is called once per frame
