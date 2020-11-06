bd9448e3422cbd6d65d06c908e47efa8adef6e37
AWieczarek <adamwie12@gmail.com>
2020-11-06 14:17:46 +0100

    Add money and buying system

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class InventoryManager : MonoBehaviour
 {
     public GameObject Car;
     public GameObject Image;
+    public GameObject PanelBlock;
 
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI price;
 
-    // Update is called once per frame
-    public void ChangeColor()
+
+
+	public void ChangeColor()
     {       
         Car.GetComponent<Renderer>().materials[1].color = Image.GetComponent<UnityEngine.UI.Image>().material.color;
         GameObject.Find("AudioManager").GetComponent<AudioManager>().carColor = Image.GetComponent<UnityEngine.UI.Image>().material.color;
     }
+    
+    public void Unlock()
+	{
+        if(GameObject.Find("PanelEq").GetComponent<Money>().money >= int.Parse(price.text))
+		{
+            GameObject.Find("PanelEq").GetComponent<Money>().money -= int.Parse(price.text);
+            PanelBlock.SetActive(false);
+		}
+	}
 }
---
+++ Assets/Money.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Money : MonoBehaviour
+{
+    public int money = 50;
+    public TextMeshProUGUI text;
+
+    void Update()
+    {
+        text.text = money.ToString() + "$";
+    }
+}
